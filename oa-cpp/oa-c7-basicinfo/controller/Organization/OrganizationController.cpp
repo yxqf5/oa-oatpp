021624cd0e2be8b937cf4a004b2e242bab018f0d
Roadinforest <whuForest@outlook.com>
2024-10-20 18:53:07 +0800

    创建1~3要点的领域模型，添加oa-c7-basicinfo/controller和domain

---
+++ oa-cpp/oa-c7-basicinfo/controller/Organization/OrganizationController.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "OrganizationController.h"
+
+OrganizationNameJsonVO::Wrapper OrganizationController::execcuteQueryOrganizationInfo(const String& name)
+{
+	return {};
+}
+
+OrganizationMemberJsonVO::Wrapper OrganizationController::execcuteQueryOrganizationMember(const String& name)
+{
+	return {};
+}
+
